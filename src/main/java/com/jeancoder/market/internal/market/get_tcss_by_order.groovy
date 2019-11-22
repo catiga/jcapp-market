b9181cd58e680755e0e6ebb624c655ea9f2522ac
jackielee <jackielee@192.168.1.9>
2019-11-22 13:47:18 +0800

    c

--- src/main/java/com/jeancoder/market/internal/market/get_tcss_by_order.groovy
+++ src/main/java/com/jeancoder/market/internal/market/get_tcss_by_order.groovy
@@ -13,9 +13,9 @@ def order_no = JC.internal.param('order_no');
 def order_oc = JC.internal.param('order_oc');
 
 //这里直接选择营销活动，需要过滤一下手机号
-def sql = 'select * from MarketRuleTcss where flag!=? and market_id in (select id from MarketInfo where flag!=? and mtype=? and pid=? order by c_time desc)';
+def sql = 'select a.*, b.title, b.info from MarketRuleTcss a, MarketInfo b where a.flag!=? and a.market_id=b.id and b.flag!=? and b.mtype=? and b.pid=? and b.mstatus=? order by c_time desc';
 
-List<MarketRuleTcss> rules = JcTemplate.INSTANCE().find(MarketRuleTcss, sql, -1, -1, order_oc, pid);
+List<MarketRuleTcss> rules = JcTemplate.INSTANCE().find(MarketRuleTcss, sql, -1, -1, order_oc, pid, '20');
 
 if(!rules || rules.empty) {
 	return SimpleAjax.available();
