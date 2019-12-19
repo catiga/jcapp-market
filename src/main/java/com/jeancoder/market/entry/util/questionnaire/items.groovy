c1fabadcd8997d3d0f3fea2ea6d788f1200e592c
jackielee <jackielee@192.168.0.100>
2019-12-19 18:17:01 +0800

    c

--- src/main/java/com/jeancoder/market/entry/util/questionnaire/items.groovy
+++ src/main/java/com/jeancoder/market/entry/util/questionnaire/items.groovy
@@ -1,6 +1,8 @@
 package com.jeancoder.market.entry.util.questionnaire
 
 import com.jeancoder.app.sdk.JC
+import com.jeancoder.core.log.JCLogger
+import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.core.result.Result
 import com.jeancoder.jdbc.JcPage
 import com.jeancoder.jdbc.JcTemplate
@@ -8,7 +10,11 @@ import com.jeancoder.market.ready.entity.QuesItem
 import com.jeancoder.market.ready.entity.QuesPack
 import com.jeancoder.market.ready.util.GlobalHolder
 
+JCLogger logger = JCLoggerFactory.getLogger('qp_items');
+
 def qp_id = JC.request.param('qp_id');
+
+logger.info('qp_id=' + qp_id + ',' + GlobalHolder.proj.id);
 
 QuesPack pack = JcTemplate.INSTANCE().get(QuesPack, 'select * from QuesPack where id=? and flag!=? and pid=?', qp_id, -1, GlobalHolder.proj.id);
 
