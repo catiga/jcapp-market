2bb9fa056b5d4540d2b31ca3675117d871f37cf5
jackielee <jackielee@192.168.72.72>
2020-01-08 10:25:09 +0800

    c

--- src/main/java/com/jeancoder/market/internal/coupon/clear_coupon_info.groovy
+++ src/main/java/com/jeancoder/market/internal/coupon/clear_coupon_info.groovy
@@ -3,24 +3,33 @@ package com.jeancoder.market.internal.coupon
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.app.sdk.source.LoggerSource
 import com.jeancoder.core.log.JCLogger
+import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.core.result.Result
+import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.market.ready.common.SimpleAjax
+import com.jeancoder.market.ready.entity.MarketMobileBuy
+import com.jeancoder.market.ready.entity.MarketMobileLimit
 import com.jeancoder.market.ready.entity.OrderCoupon
 import com.jeancoder.market.ready.service.OrderCouponServer
 import com.jeancoder.market.ready.util.JackSonBeanMapper
 import com.jeancoder.market.ready.util.StringUtil
 
 Result result = new Result();
-JCLogger Logger = LoggerSource.getLogger(this.getClass().getName())
+JCLogger Logger = JCLoggerFactory.getLogger(this.getClass().getName())
 def p = JC.internal.param("orders");
 def pid = JC.internal.param("pid");
 Logger.info("orders__" + p)
+
+if (StringUtil.isEmpty(p)||StringUtil.isEmpty(pid)) {
+	return SimpleAjax.notAvailable('参数不可为空');
+}
+List<OrderCoupon> resultList = new ArrayList();
+
+String [] list = p.split(";"); //参数为："编号,oc"
+
+def a_time = Calendar.getInstance().getTime();
+def time_diff = 15*60*1000L;	//15分钟
 try {
-	if (StringUtil.isEmpty(p)||StringUtil.isEmpty(pid)) {
-		return SimpleAjax.notAvailable('参数不可为空');
-	}
-	List<OrderCoupon> resultList = new ArrayList();
-	String [] list = p.split(";");//参数为："编号+oc"
 	for (int i = 0 ; i<list.length;i++) {
 		def item = list[i].split(",");
 		OrderCoupon order = new OrderCoupon();
@@ -29,10 +38,27 @@ try {
 		order.pid = new BigInteger(pid);
 		List<OrderCoupon> result_data  = OrderCouponServer.INSTANSE.update_order_coupon(order);
 		resultList.addAll(result_data);
+		
+		//修改并还原手机号对应的订单信息
+		def sql = 'select * from MarketMobileBuy where flag!=? and order_no=? and oc=? order by a_time desc limit 1, 10';
+		try {
+			List<MarketMobileBuy> mobile_markets = JcTemplate.INSTANCE().find(MarketMobileBuy, sql, -1, item[0], item[1]);
+			if(mobile_markets) {
+				for(x in mobile_markets) {
+					if((a_time.getTime() - x.a_time.getTime())<=time_diff) {
+						//小于15分钟，直接重置还原
+						x.flag = -1;
+						JcTemplate.INSTANCE().update(x);
+					}
+				}
+			}
+		} catch(any) {
+			Logger.error("restore_market_rule_fail:" + item[0], any);
+		}
 	}
-	Logger.info('coupon_data=' + JackSonBeanMapper.listToJson(resultList) );
-	return SimpleAjax.available("",resultList);
 } catch (Exception e) {
-	Logger.error("修改卡券状态失败", e);
-	return SimpleAjax.notAvailable("修改卡券状态失败");
+	Logger.error("restore_coupon_fail:", e);
+	//return SimpleAjax.notAvailable("修改卡券状态失败");
 }
+
+return SimpleAjax.available("",resultList);
